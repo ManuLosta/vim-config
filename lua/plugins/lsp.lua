0b3bd886bf74dc845e7422ef1c9bd3dc0d816b7f
ManuLosta <mlostalom@gmail.com>
2024-05-05 17:21:02 -0300

    add autocompletion

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,35 +1,37 @@
 return {
-  "neovim/nvim-lspconfig",
-  dependencies = {
-    { "williamboman/mason.nvim", config = true },
-    "williamboman/mason-lspconfig.nvim",
-    'WhoIsSethDaniel/mason-tool-installer.nvim',
-    { "j-hui/fidget.nvim", opts = {} },
-    { "folke/neodev.nvim", opts = {} }
-  },
-  config = function()
-    require("mason").setup()
-    
-    require("mason-tool-installer").setup({
-      ensure_installed = {
-        "stylua",
-      }
-    })
+	"neovim/nvim-lspconfig",
+	dependencies = {
+		{ "williamboman/mason.nvim", config = true },
+		"williamboman/mason-lspconfig.nvim",
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+		{ "j-hui/fidget.nvim", opts = {} },
+		{ "folke/neodev.nvim", opts = {} },
+	},
+	config = function()
+		require("mason").setup()
 
-    local servers = {
-      lua_ls,
-      tsserver,
-      pyright,
-      gopls
-    }
+		local servers = {
+			lua_ls = {},
+			tsserver = {},
+			clangd = {},
+		}
 
-    require("mason-lspconfig").setup({
-      handlers = {
-        function(server_name)
-          local server = servers[server_name] or {}
-          require("lsp-config")[server_name].setup(server)
-        end
-      }
-    })
-  end
+		local ensure_installed = vim.tbl_keys(servers or {})
+		vim.list_extend(ensure_installed, {
+			"stylua", -- Used to format Lua code
+			"prettier",
+			"clang-format",
+		})
+
+		require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
+
+		require("mason-lspconfig").setup({
+			handlers = {
+				function(server_name)
+					local server = servers[server_name] or {}
+					require("lspconfig")[server_name].setup(server)
+				end,
+			},
+		})
+	end,
 }
