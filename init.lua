16dd4e1e07b01ac94a1924d3696d17a3a3aab1cd
ManuLosta <mlostalom@gmail.com>
2024-05-08 14:02:56 -0300

    rename vim options file

--- init.lua
+++ init.lua
@@ -11,6 +11,6 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("vim-options")
+require("options")
 require("keymaps")
 require("lazy").setup({ { import = "plugins" } })
